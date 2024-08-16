fe468e36450ac1445f855fc715072559f87aae81
Brunno Silva <brunnoqsilva@gmail.com>
2024-08-16 08:19:44 -0300

    feat: Fix JSON parsing issue in JsonUtil

--- src/main/java/br/com/calculator/utils/json/JsonUtil.java
+++ src/main/java/br/com/calculator/utils/json/JsonUtil.java
@@ -47,4 +47,6 @@ public final class JsonUtil {
             throw new RuntimeException ("JSON inválido.");
         }
     }
+
+    
 }
--- src/test/java/br/com/calculator/utils/json/JsonUtilTest.java
+++ src/test/java/br/com/calculator/utils/json/JsonUtilTest.java
@@ -4,10 +4,11 @@ import static br.com.calculator.cenarios.JsonUtilCenarios.JSON_OPERATION_VALIDO;
 import static br.com.calculator.cenarios.JsonUtilCenarios.INPUT_LIST_VALIDA;
 import static br.com.calculator.cenarios.JsonUtilCenarios.OUTPUT_LIST_VALIDA;
 import static br.com.calculator.cenarios.JsonUtilCenarios.JSON_IMPOSTO_VALIDO;
+import static br.com.calculator.cenarios.JsonUtilCenarios.JSON_INVALIDO;
 import static br.com.calculator.utils.json.JsonUtil.jsonToList;
 import static br.com.calculator.utils.json.JsonUtil.listToJson;
 import static org.junit.jupiter.api.Assertions.assertEquals;
-
+import static org.junit.jupiter.api.Assertions.assertThrows;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -25,4 +26,9 @@ public class JsonUtilTest {
         assertEquals(JSON_IMPOSTO_VALIDO, listToJson(OUTPUT_LIST_VALIDA));
     }
 
+    @Test
+    @DisplayName("Deve lançar exceção ao tentar converter json invalido para lista de operacoes.")
+    void converter_json_invalido_lanca_exception() {
+        assertThrows(RuntimeException.class, () -> jsonToList(JSON_INVALIDO));
+    }
 }
