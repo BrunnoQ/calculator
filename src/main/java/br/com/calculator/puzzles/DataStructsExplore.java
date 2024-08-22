e40646cf847041d97613172799cd215833bf8a7b
Brunno Silva <brunnoqsilva@gmail.com>
2024-08-21 21:21:19 -0300

    feat: Add DataStructsExplore class for exploring data structures

---
+++ src/main/java/br/com/calculator/puzzles/DataStructsExplore.java
@@ -0,0 +1,16 @@
+package br.com.calculator.puzzles;
+
+public class DataStructsExplore {
+
+    public static void main(String[] args) {
+
+        /**
+         * Classes java que implementam List
+         * 
+         */
+
+
+
+    }
+
+}
